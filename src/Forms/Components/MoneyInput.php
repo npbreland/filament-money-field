aba9c8e8fcc1e49e050b4246ee84d76b36810478
Nick Breland <npbreland@tutanota.com>
2024-02-14 11:40:40 +0000

    wip

--- src/Forms/Components/MoneyInput.php
+++ src/Forms/Components/MoneyInput.php
@@ -47,6 +47,25 @@ class MoneyInput extends TextInput
     {
         $formattingRules = MoneyFormatter::getFormattingRules($component->getLocale());
         $this->prefix($formattingRules->currencySymbol);
+        $this->rules([
+            function () use ($component) {
+                return function (string $attribute, $value, \Closure $fail) use ($component) {
+                    $value = MoneyFormatter::parseDecimal(
+                        $value, 
+                        $component->getCurrency()->getCode(),
+                        $component->getLocale()
+                    );
+
+                    if ($value < 0) {
+                        $fail('The :attribute must be greater than 0.');
+                    } 
+                    if ($value > 4_294_967_295) {
+                        $fail('The :attribute must be less than ' . 4_294_967_295 . '.');
+                    }
+                };
+            }
+        ]);
+
         if (config('filament-money-field.use_input_mask')) {
             $this->mask(RawJs::make('$money($input, \'' . $formattingRules->decimalSeparator . '\', \'' . $formattingRules->groupingSeparator . '\', ' . $formattingRules->fractionDigits . ')'));
         }
