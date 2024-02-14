af3e8407f44375e77656ddd1d257be9651066ae5
Nick Breland <npbreland@tutanota.com>
2024-02-14 12:31:10 +0000

    wip

--- src/Forms/Components/MoneyInput.php
+++ src/Forms/Components/MoneyInput.php
@@ -53,37 +53,57 @@ class MoneyInput extends TextInput
         }
     }
 
+    /* public function maxValue($max): static */
+    /* { */
+    /*     $this->rule('max_value', function ($attribute, $value, $fail) use ($max) { */
+
+    /*         $value = MoneyFormatter::parseDecimal( */
+    /*             $value, */ 
+    /*             $this->getCurrency()->getCode(), */
+    /*             $this->getLocale() */
+    /*         ); */
+
+    /*         if ($value > $max) { */
+    /*             $fail('The :attribute must be less than ' . $max . '.'); */
+    /*         } */
+    /*     }); */
+    /*     return $this; */
+    /* } */
+
+    /* public function minValue($min): static */
+    /* { */
+    /*     $this->rule('min_value', function ($attribute, $value, $fail) use ($min) { */
+
+    /*         $value = MoneyFormatter::parseDecimal( */
+    /*             $value, */ 
+    /*             $this->getCurrency()->getCode(), */
+    /*             $this->getLocale() */
+    /*         ); */
+
+    /*         if ($value < $min) { */
+    /*             $fail("The :attribute must be greater than " . $min . '.'); */
+    /*         } */
+    /*     }); */
+    /*     return $this; */
+    /* } */
+
     public function maxValue($max): static
     {
-        $this->rule('max_value', function ($attribute, $value, $fail) use ($max) {
+        $this->rule(static function (MoneyInput $component, mixed $state) use ($max) {
+            return function (string $attribute, mixed $value, \Closure $fail) use ($component, $state, $max) {
 
-            $value = MoneyFormatter::parseDecimal(
-                $value, 
-                $this->getCurrency()->getCode(),
-                $this->getLocale()
-            );
+                $value = MoneyFormatter::parseDecimal(
+                    $state, 
+                    $component->getCurrency()->getCode(),
+                    $component->getLocale()
+                );
 
-            if ($value > $max) {
-                $fail('The :attribute must be less than ' . $max . '.');
-            }
+                if ($value > $max) {
+                    $fail(__($component->getValidationMessages()['max'], ['attribute' => $component->getValidationAttribute(), 'max' => $max]));
+                }
+            };
         });
-        return $this;
-    }
 
-    public function minValue($min): static
-    {
-        $this->rule('min_value', function ($attribute, $value, $fail) use ($min) {
-
-            $value = MoneyFormatter::parseDecimal(
-                $value, 
-                $this->getCurrency()->getCode(),
-                $this->getLocale()
-            );
-
-            if ($value < $min) {
-                $fail('The :attribute must be greater than ' . $min . '.');
-            }
-        });
         return $this;
     }
 }
